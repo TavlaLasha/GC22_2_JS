eca900cf8ac87c60bc39698b145077f69b83930c
LAB316 Student16 <LAB316_Student16@gau.local>
2019-09-27 15:38:40 +0400

    My Second Commit in Week 2

---
+++ Week_2/Week2_2/script.js
@@ -0,0 +1,9 @@
+function textInDiv(id, text) {
+    div = document.getElementById(id);
+    div.innerText = text;
+}
+function htmlInDiv(id, text){
+    div = document.getElementById(id);
+    div.innerHTML = "<h2>"+text+"</h2>";
+    div.style.borderBottom = "solid green 3px";
+}
