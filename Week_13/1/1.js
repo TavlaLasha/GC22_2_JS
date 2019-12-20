f2b8fc7508f6b5e6dfeecb3658d1ac6cce1816f5
Lasha <lashatavlalashvili@yahoo.com>
2019-12-20 11:50:25 +0400

    My 14th Week Home Commit

--- Week_13/1/1.js
+++ Week_13/1/1.js
@@ -1,13 +1,22 @@
-var div = document.getElementById("info");
-div.innerHTML = "ეს ინფორმაცია შეგიძლიათ წაშალოთ.";
-document.getElementById("del").addEventListener("click", function(){
-    hold = div.innerHTML;
-    div.innerHTML = "";
-})
-document.getElementById("wrt").addEventListener("click", function(){
-    div.innerHTML = hold;
-})
-document.getElementById("chng").addEventListener("click", function(){
-    div.innerHTML = prompt("შეიყვანეთ ინფორმაცია:");
-    hold = div.innerHTML;
-})
+function dnt(){
+    year = document.getElementById("s1");
+    date = document.getElementById("s2");
+    tm = document.getElementById("d4");
+
+    year.innerHTML = "";
+    date.innerHTML = "";
+    tm.innerHTML = "";
+    time = new Date;
+    year.innerHTML += time.getFullYear();
+    date.innerHTML += "/" + zeroAdder(time.getMonth()) + "/" + zeroAdder(time.getDate());
+    tm.innerHTML += zeroAdder(time.getHours()) + " : " + zeroAdder(time.getMinutes()) + " : " + zeroAdder(time.getSeconds());
+}
+function zeroAdder(time){
+    if(time <= 9){
+        return '0' + time;
+    }
+    else{
+        return time;
+    }
+}
+setInterval(dnt, 1000);
